751eb92a5ceacce2d0a8d45e369476d654614eb5
Hatami.Esmaeil <Hatami.Esmaeil@TEK001P10665>
2023-02-06 09:22:45 +0330

    FirstCommit

---
+++ ExampleApiMetric/Program.cs
@@ -0,0 +1,47 @@
+using App.Metrics.AspNetCore;
+using App.Metrics.Formatters.Prometheus;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.Configure<KestrelServerOptions>(option =>
+{
+    option.AllowSynchronousIO = true; 
+});
+
+builder.Services.AddMetrics();
+builder.Host.UseMetricsWebTracking();
+builder.Host.UseMetrics(option =>
+{
+    option.EndpointOptions = endpointsOptions =>
+    {
+        endpointsOptions.MetricsTextEndpointOutputFormatter = new MetricsPrometheusTextOutputFormatter();
+        endpointsOptions.MetricsEndpointOutputFormatter = new MetricsPrometheusProtobufOutputFormatter();
+        endpointsOptions.EnvironmentInfoEndpointEnabled = false;
+
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
